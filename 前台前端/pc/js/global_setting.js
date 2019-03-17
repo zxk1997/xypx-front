23ef82dfc91a64538e6a7cb5693199cc344af7c1
97lulala <dnf2503@qq.com>
2019-03-17 15:07:46 +0800

    修改了readme

--- 前台前端/pc/js/global_setting.js
+++ 前台前端/pc/js/global_setting.js
@@ -1,5 +1,5 @@
 ﻿var global_url = "http://localhost:8080/xypx-normal";
 
-var global_upimg = "http://120.78.83.14:2000/pc/upload?callback="+location.origin+"/pc/redirect.html";
+var global_upimg = "http://localhost:2000/pc/upload?callback="+location.origin+"/pc/redirect.html";
 
-var global_upimg_simple  = "http://120.78.83.14:2000/upload";
+var global_upimg_simple  = "http://localhost:2000/upload";
--- 后台前端/README.md
+++ 后台前端/README.md
@@ -1,8 +1,6 @@
-# one
+# 校园蒲行后台前端
 
-> test
-
-## Build Setup
+#### 启动命令(项目使用vue cli生成)
 
 ``` bash
 # install dependencies
@@ -15,4 +13,4 @@ npm run dev
 npm run build
 ```
 
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+##### 原蒲行后台直接使用网上的模板，比较臃肿，修改起来很困难，于是学了下vue的一些基础，配合element ui搭建出了一个基本的后台
